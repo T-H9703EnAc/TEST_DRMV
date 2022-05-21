1d8f5acdacd1d16a1ec1ea11bc8e255906e3fb6b
T-H9703EnAc <t.h.1997.m.engineer.ac@gmail.com>
2022-05-22 02:36:50 +0900

    BootStrap5追加

--- front/vue_project/src/App.vue
+++ front/vue_project/src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <router-view></router-view>
+    <div id="app">
+      <Header/>
+      <router-view name="Header"></router-view>
+      <router-view></router-view>
+    </div>
 </template>
+
+<script>
+  import Header from './components/Header'
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
+  }
+</script>
