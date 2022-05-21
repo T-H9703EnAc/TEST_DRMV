3bb25518c028d7bd6c6eeee0550fb685f5fe8ac4
T-H9703EnAc <t.h.1997.m.engineer.ac@gmail.com>
2022-05-22 03:41:49 +0900

    axios追加

--- front/vue_project/src/components/Header.vue
+++ front/vue_project/src/components/Header.vue
@@ -9,6 +9,7 @@
         <div class="navbar-nav">
             <a class="nav-link active" aria-current="page" href="/">Home</a>
             <a class="nav-link" href="/hello">hello</a>
+            <a class="nav-link" href="/search">search</a>
             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </div>
         </div>
--- front/vue_project/src/router/router.js
+++ front/vue_project/src/router/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Hello from '../pages/hello/Hello.vue'
+import Search from '../pages/search/Search.vue'
 
 const routes = [
     { 
@@ -12,6 +13,11 @@ const routes = [
         path: '/hello', 
         name: 'Hello',
         component: Hello
+    },
+    {
+        path: '/search', 
+        name: 'Search',
+        component: Search
     }
 ]
 
